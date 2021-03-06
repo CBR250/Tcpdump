19d857ec831e440f61d8ea6b74f40ed2f01b0c84
Jumbo Fang <f.kcsmart@gmail.com>
2021-03-06 10:50:07 +0000

    Add Embedded system package

---
+++ Embedded/tcpdump/env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "************ set environemt ***************************"
+export PATH=/share/rlx/asdk-5.4.1-a53-EL-4.4-g2.23-a64nut-170801-r1/bin/:$PATH
+export CROSS_COMPILE=aarch64-linux-gnu-
+
+echo $PATH
